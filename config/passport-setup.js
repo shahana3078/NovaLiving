286abe88a3b82f96024c3e79b004aac726186ea9
shahana <shahana3078@gmail.com>
2025-04-22 09:40:16 +0530

    updated google callbackURL

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -5,7 +5,7 @@ const User = require('../Models/userSchema');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback' 
+    callbackURL: process.env.GOOGLE_CALLBACK_URL 
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await User.findOne({ googleId: profile.id });
